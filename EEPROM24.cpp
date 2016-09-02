632e1c954be4204af5e5a33fa4b95f63287fe219
Matt Jenkins <matt@majenko.co.uk>
2016-09-02 13:45:26 +0100

    Fixed address bug and added more chips

--- EEPROM24.cpp
+++ EEPROM24.cpp
@@ -9,19 +9,20 @@ void EEPROM24::begin(uint8_t addr) {
     Wire.begin();
 }
 
-size_t EEPROM24::write(uint32_t addr, uint8_t byte) {
+size_t EEPROM24::write(uint32_t addr, uint8_t b) {
     if (addr >= _bytes) return 0;
 
     Wire.beginTransmission(_i2caddr);
-    Wire.write((uint8_t)((addr > 8)  & 0xFF));
+    Wire.write((uint8_t)((addr >> 8)  & 0xFF));
     Wire.write((uint8_t)(addr & 0xFF));
-    Wire.write(byte);
-    Wire.endTransmission();
+    Wire.write(b);
+    int r = Wire.endTransmission();
 
     Wire.beginTransmission(_i2caddr);
     int res = Wire.endTransmission();
-    uint32_t to = 1000;
+    uint32_t to = 100;
     while (res != 0) {
+        delay(1);
         Wire.beginTransmission(_i2caddr);
         res = Wire.endTransmission();
         to--;
@@ -32,11 +33,82 @@ size_t EEPROM24::write(uint32_t addr, uint8_t byte) {
     return 1;
 }
 
+size_t EEPROM24::write(uint32_t addr, const uint8_t *bytes, uint32_t len) {
+
+//for (int i = 0; i < len; i++) {
+//    write(addr + i, bytes[i]);
+//}
+//return len;
+
+    if (addr >= _bytes) {
+        return 0;
+    }
+
+    Wire.beginTransmission(_i2caddr);
+    Wire.write((uint8_t)((addr >> 8)  & 0xFF));
+    Wire.write((uint8_t)(addr & 0xFF));
+
+    uint32_t written = 0;
+
+    for (uint32_t b = 0; b < len; b++) {
+        Wire.write(bytes[b]);
+        written++;
+        addr++;
+        if (addr >= _bytes) { // Terminate early - run out of space
+            int res = Wire.endTransmission();
+            uint32_t to = 100;
+            while (res != 0) {
+                delay(1);
+                Wire.beginTransmission(_i2caddr);
+                res = Wire.endTransmission();
+                to--;
+                if (to == 0) {
+                    return written;
+                }
+            }
+            return written;
+        }
+        if ((addr & (_page - 1)) == 0) { // Looped to start of page
+            int res = Wire.endTransmission();
+            uint32_t to = 100;
+            while (res != 0) {
+                delay(1);
+                Wire.beginTransmission(_i2caddr);
+                res = Wire.endTransmission();
+                to--;
+                if (to == 0) {
+                    return written;
+                }
+            }
+            Wire.beginTransmission(_i2caddr);
+            Wire.write((uint8_t)((addr >> 8)  & 0xFF));
+            Wire.write((uint8_t)(addr & 0xFF));
+        }
+    }
+    Wire.endTransmission();
+    int res = Wire.endTransmission();
+    uint32_t to = 10;
+    while (res != 0) {
+        delay(1);
+        Wire.beginTransmission(_i2caddr);
+        res = Wire.endTransmission();
+        to--;
+        if (to == 0) {
+            return written;
+        }
+    }
+    return written;
+}
+
+size_t EEPROM24::write(uint32_t addr, const char *str) {
+    return write(addr, (const uint8_t *)str, strlen(str));
+}
+
 uint8_t EEPROM24::read(uint32_t addr) {
     if (addr >= _bytes) return 0;
 
     Wire.beginTransmission(_i2caddr);
-    Wire.write((uint8_t)((addr > 8)  & 0xFF));
+    Wire.write((uint8_t)((addr >> 8)  & 0xFF));
     Wire.write((uint8_t)(addr & 0xFF));
     Wire.endTransmission(false);
     Wire.requestFrom(_i2caddr, (uint8_t)1);
--- EEPROM24.h
+++ EEPROM24.h
@@ -8,20 +8,83 @@ class EEPROM24 {
     protected:  
         uint8_t _i2caddr;
         uint32_t _bytes;
+        uint32_t _page;
 
     public:
-        EEPROM24(uint8_t a, uint32_t b) : _i2caddr(a), _bytes(b) {}
+        EEPROM24(uint8_t a, uint32_t b, uint32_t p) : _i2caddr(a), _bytes(b), _page(p) {}
 
         void begin();
         void begin(uint8_t addr);
-        size_t write(uint32_t addr, uint8_t byte);
+        size_t write(uint32_t addr, uint8_t b);
+        size_t write(uint32_t addr, const char *str);
+        size_t write(uint32_t addr, const uint8_t *bytes, size_t len);
         uint8_t read(uint32_t addr);
 };
 
+class EEPROM24_512 : public EEPROM24 {
+    public:
+        EEPROM24_512() : EEPROM24(0x50, 65536, 16) {}
+        EEPROM24_512(uint8_t addr) : EEPROM24(addr, 65536, 16) {}
+};
+                
 class EEPROM24_256 : public EEPROM24 {
     public:
-        EEPROM24_256() : EEPROM24(0x50, 32768) {}
-        EEPROM24_256(uint8_t addr) : EEPROM24(addr, 32768) {}
+        EEPROM24_256() : EEPROM24(0x50, 32768, 16) {}
+        EEPROM24_256(uint8_t addr) : EEPROM24(addr, 32768, 16) {}
+};
+                
+class EEPROM24_128 : public EEPROM24 {
+    public:
+        EEPROM24_128() : EEPROM24(0x50, 16384, 16) {}
+        EEPROM24_128(uint8_t addr) : EEPROM24(addr, 16384, 16) {}
+};
+                
+class EEPROM24_64 : public EEPROM24 {
+    public:
+        EEPROM24_64() : EEPROM24(0x50, 8192, 16) {}
+        EEPROM24_64(uint8_t addr) : EEPROM24(addr, 8192, 16) {}
+};
+                
+class EEPROM24_32 : public EEPROM24 {
+    public:
+        EEPROM24_32() : EEPROM24(0x50, 4096, 16) {}
+        EEPROM24_32(uint8_t addr) : EEPROM24(addr, 4096, 16) {}
+};
+                
+class EEPROM24_16 : public EEPROM24 {
+    public:
+        EEPROM24_16() : EEPROM24(0x50, 2048, 16) {}
+        EEPROM24_16(uint8_t addr) : EEPROM24(addr, 2048, 16) {}
+};
+                
+class EEPROM24_08 : public EEPROM24 {
+    public:
+        EEPROM24_08() : EEPROM24(0x50, 1024, 16) {}
+        EEPROM24_08(uint8_t addr) : EEPROM24(addr, 1024, 16) {}
+};
+                
+class EEPROM24_04 : public EEPROM24 {
+    public:
+        EEPROM24_04() : EEPROM24(0x50, 512, 16) {}
+        EEPROM24_04(uint8_t addr) : EEPROM24(addr, 512, 16) {}
+};
+                
+class EEPROM24_02 : public EEPROM24 {
+    public:
+        EEPROM24_02() : EEPROM24(0x50, 256, 8) {}
+        EEPROM24_02(uint8_t addr) : EEPROM24(addr, 256, 8) {}
+};
+                
+class EEPROM24_01 : public EEPROM24 {
+    public:
+        EEPROM24_01() : EEPROM24(0x50, 128, 8) {}
+        EEPROM24_01(uint8_t addr) : EEPROM24(addr, 128, 8) {}
+};
+                
+class EEPROM24_00 : public EEPROM24 {
+    public:
+        EEPROM24_00() : EEPROM24(0x50, 16, 1) {}
+        EEPROM24_00(uint8_t addr) : EEPROM24(addr, 16, 1) {}
 };
                 
 #endif
